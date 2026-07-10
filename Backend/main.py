c7337ce01e6c8ddecfeedefa961b6b9851da3608
Harini-Hub-max <harini.s699@gmail.com>
2026-07-10 12:28:48 +0530

    Update main.py imports

--- Backend/main.py
+++ Backend/main.py
@@ -1,56 +1,41 @@
 from fastapi import FastAPI
 from database.db import engine
 
-<<<<<<< HEAD
-from routes import user
-from routes import profile
-from routes import sleep
-from routes import wake_goal
-from routes import habit
-=======
-from routes import user, alarm
->>>>>>> origin/feature/alarm-management-v2
+from routes import user, alarm, profile, sleep, wake_goal, habit, challenge, performance, verification
 
 from models.user import User
-<<<<<<< HEAD
-from models.profile import Profile   
+from models.alarm import Alarm
+from models.profile import Profile
 from models.sleep import Sleep
 from models.wake_goal import WakeGoal
 from models.habit import Habit
-
-User.metadata.create_all(bind=engine)
-Profile.metadata.create_all(bind=engine)   
-Sleep.metadata.create_all(bind=engine)
-WakeGoal.metadata.create_all(bind=engine)
-Habit.metadata.create_all(bind=engine)
-app = FastAPI()
-
-app.include_router(user.router)
-app.include_router(profile.router)
-app.include_router(sleep.router)
-app.include_router(wake_goal.router)
-app.include_router(habit.router)
-=======
-from models.alarm import Alarm
+from models.challenge import Challenge
+from models.performance import Performance
+from models.verification import WakeupVerification
 
 User.metadata.create_all(bind=engine)
 Alarm.metadata.create_all(bind=engine)
+Profile.metadata.create_all(bind=engine)
+Sleep.metadata.create_all(bind=engine)
+WakeGoal.metadata.create_all(bind=engine)
+Habit.metadata.create_all(bind=engine)
+Challenge.metadata.create_all(bind=engine)
+Performance.metadata.create_all(bind=engine)
+WakeupVerification.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(alarm.router)
+app.include_router(profile.router)
+app.include_router(sleep.router)
+app.include_router(wake_goal.router)
+app.include_router(habit.router)
+app.include_router(challenge.router)
+app.include_router(performance.router)
+app.include_router(verification.router)
 
->>>>>>> origin/feature/alarm-management-v2
 
 @app.get("/")
 def home():
-    return {
-        "message": "Backend is successfully running"
-<<<<<<< HEAD
-    }
-
-print("test change")
-=======
-    }
->>>>>>> origin/feature/alarm-management-v2
+    return {"message": "Backend is successfully running"}
